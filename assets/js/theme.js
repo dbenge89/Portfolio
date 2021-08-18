7f3c2c951101d849871b49b2202c22effd35e127
dbenge89 <davidcbenge89@gmail.com>
2021-08-18 15:47:12 -0400

    Finally, more proofreading but it does work. Noice

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,5 @@
-document.querySelectorAll(".datepicker").forEach(function (field) {
-  var picker = new Pikaday({
-    field: field,
-  });
+document.querySelectorAll('.datepicker').forEach(function(field) {
+	var picker = new Pikaday({
+		field: field
+	});
 });
